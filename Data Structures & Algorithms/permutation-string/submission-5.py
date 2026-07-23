9c5d46f4e3cdea2632f962c3bcdf472e8cc155d9
Hayden (Khang) Luong <haydenluong07@gmail.com>
2026-07-23 14:32:22 +0700

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # lecabee
+
+        if len(s1) > len(s2):
+            return False
+
+        cnt_s1 = Counter(s1)
+        track = Counter()
+
+        l = 0
+        for r in range(len(s2)):
+            track[s2[r]] += 1
+            
+            if (r - l + 1) > len(s1): 
+                track[s2[l]] -= 1
+                if track[s2[l]] == 0:
+                    del track[s2[l]]
+                l += 1
+            
+            if track == cnt_s1:
+                return True
+        return False
+
+        
+
+
+
+
